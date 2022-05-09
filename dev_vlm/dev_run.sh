2cea0823bba805b020cbb05fd8a90df5cb9d9d44
Dima Sivov <dsivov@gmail.com>
2022-05-09 11:16:51 +0000

    docker with vscode

--- dev_vlm/dev_run.sh
+++ dev_vlm/dev_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export SHELL=/bin/bash
 rm -rf /storage/lost+found
-export PYTHONPATH=/notebooks/:/notebooks/nebula3_database
+export PYTHONPATH=/notebooks/:/notebooks/nebula3_database:/notebooks/nebula3_vlm
 export PATH=$PATH:/opt/conda/bin
 export TRANSFORMERS_CACHE=/nonetbooks/models
 if  [ ! -d "/notebooks" ]
@@ -17,10 +17,12 @@ then
    cd /notebooks && \
    git clone https://github.com/NEBULA3PR0JECT/nebula3_database.git
    git clone https://github.com/NEBULA3PR0JECT/nebula3_vlm.git
+   git clone https://github.com/NEBULA3PR0JECT/nebula3_videoprocessing.git
+   git clone https://github.com/NEBULA3PR0JECT/nebula3_playground.git
 fi
-source activate nebula
 chmod -R a+w /notebooks
 if [ -z "$JUPYTER_TOKEN" ]; then
+    source activate nebula
     JUPYTER_TOKEN=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 48 | head -n 1)
 fi
 if [ "$1" = "code" ]
