44922403180f89a5ce79feae26710d3dbe8832f1
Dima Sivov <dsivov@gmail.com>
2022-06-20 08:25:13 +0300

    add vidil Docker

--- vidil/install_pkg_minimum.sh
+++ vidil/install_pkg_minimum.sh
@@ -1,8 +1,12 @@
+apt-key del 7fa2af80
+wget https://developer.download.nvidia.com/compute/cuda/repos/$distro/$arch/cuda-keyring_1.0-1_all.deb
+dpkg -i cuda-keyring_1.0-1_all.deb
 apt update
 apt install lsof
 
 # conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch
-
+which pip
+which conda
 # pip install -U pip setuptools wheel
 # pip install -U 'spacy[cuda112]'
 python -m spacy download en_core_web_sm
